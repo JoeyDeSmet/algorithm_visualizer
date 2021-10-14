be3d44dc26675757db4dbac25064fe9bc2eb32a9
Joey <joey@de-smet.vlaanderen>
2021-10-14 17:43:48 +0200

    Added uncomplete nav-bar

---
+++ components/navbar.js
@@ -0,0 +1,10 @@
+app.component('nav-bar', {
+    template: 
+    /*html*/
+    `<div class="nav">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Not home</a></li>
+        </ul>
+    </div>`
+})
