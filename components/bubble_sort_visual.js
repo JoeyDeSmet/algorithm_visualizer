9d98fc5e9fde44137c6363d97124373cabfbb6c5
Joey <joey@de-smet.vlaanderen>
2021-10-15 10:47:00 +0200

    Made bubble sort visualization mor clear

--- components/bubble_sort_visual.js
+++ components/bubble_sort_visual.js
@@ -4,14 +4,24 @@ app.component('bubble-sort', {
     `
     <div class="bubble">
         <div class="graph">
-            <div v-if="!sorting" v-for="(elments, index) in array" v-bind:style="{ height: 10 * array[index] + 'px', width: 50 + 'px' ,'background-color': '#4e4e4e' }"></div>
-            <div v-else v-for="(elments, index) in toSortArray" v-bind:style="{ height: 10 * array[index] + 'px', width: 50 + 'px' ,'background-color': '#4e4e4e' }"></div>
+            <div v-if="!sorting" v-for="(elments, index) in array" :style="{ height: 10 * array[index] + 'px', width: 50 + 'px' ,'background-color': '#4e4e4e' }"></div>
+            <div v-else v-for="(elments, index) in toSortArray" 
+            :style="index == selected.index ? { height: 10 * array[index] + 'px', width: 50 + 'px', 'background-color' : 'red'}
+            : { height: 10 * array[index] + 'px', width: 50 + 'px', 'background-color' : '#4e4e4e'}">
+            </div>
         </div>
 
         <div class="options">
             <div>
                 <label for="number">Elements in array</label>
-                <select id="number" v-model="number">
+                <select id="number" v-model="number" v-if="!sorting">
+                    <option>25</option>
+                    <option>20</option>
+                    <option>15</option>
+                    <option>10</option>
+                    <option>5</option>
+                </select>
+                <select id="number" v-model="number" v-else disabled>
                     <option>25</option>
                     <option>20</option>
                     <option>15</option>
@@ -27,7 +37,7 @@ app.component('bubble-sort', {
             
             <div>
                 <button class="button"
-                @click="shuffle">Shuffle</button>
+                @click="stop">Stop</button>
             </div>
         </div>
     </div>
@@ -36,16 +46,20 @@ app.component('bubble-sort', {
         return {
             number: 25,
             toSortArray: [],
-            sorting: false
+            sorting: false,
+            selected: {
+                index: 0
+            }
         }
     },
     methods: {
-        startSort() {
+        async startSort() {
             if (this.sorting) return
             this.sorting = true
-            this.toString = sort(this.toSortArray)
+            await sort(this.toSortArray, this.selected)
+            this.sorting = false
         },
-        shuffle() {
+        stop() {
             this.sorting = false
         }
     },
--- src/bubble_sort/bubbleSort.js
+++ src/bubble_sort/bubbleSort.js
@@ -2,11 +2,12 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function sort(array) {
+async function sort(array, selected) {
     for(var i = 0; i < array.length; i++){
         let swaped = false
         for(var j = 0; j < array.length - 1 - i; j++) {
             await sleep(200)
+            selected.index = j + 1
             if (array[j] > array[j + 1]) {
                 swaped = true
                 let temp = array[j]
