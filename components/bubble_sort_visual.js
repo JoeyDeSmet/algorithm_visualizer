6e8b47ae8c6a340b75ed963fd7eef360961dbaef
Joey <joey@de-smet.vlaanderen>
2021-10-16 13:52:34 +0200

    Cleaned up code

--- components/bubble_sort_visual.js
+++ components/bubble_sort_visual.js
@@ -1,47 +1,4 @@
 app.component('bubblesort', {
-    template:
-    /*html*/
-    `
-    <div class="bubble">
-        <div class="graph">
-            <div v-if="!sorting" v-for="(elments, index) in array" :style="{ height: 10 * array[index] + 'px', width: 50 + 'px' ,'background-color': '#4e4e4e', 'border-width' : 0.5 + 'px', 'border-color' : 'black', 'border-style' : 'solid' }"></div>
-            <div v-else v-for="(elments, index) in toSortArray" 
-            :style="index == selected.index ? { height: 10 * array[index] + 'px', width: 50 + 'px', 'background-color' : 'red', 'border-width' : 0.5 + 'px', 'border-color' : 'black', 'border-style' : 'solid'}
-            : { height: 10 * array[index] + 'px', width: 50 + 'px', 'background-color' : '#4e4e4e', 'border-width' : 0.5 + 'px', 'border-color' : 'black', 'border-style' : 'solid'}">
-            </div>
-        </div>
-        <div class="options">
-            <div>
-                <label for="number">Elements in array</label>
-                <select id="number" v-model="number" v-if="!sorting">
-                    <option>25</option>
-                    <option>20</option>
-                    <option>15</option>
-                    <option>10</option>
-                    <option>5</option>
-                </select>
-                <select id="number" v-model="number" v-else disabled>
-                    <option>25</option>
-                    <option>20</option>
-                    <option>15</option>
-                    <option>10</option>
-                    <option>5</option>
-                </select>
-            </div>
-                
-            <div>
-                <button class="button"
-                @click="sort(this.toSortArray)">Sort!</button>
-            </div>
-            
-            <div>
-                <button class="button"
-                @click="stop">Stop</button>
-            </div>
-
-        </div>
-    </div>
-    `,
     data() {
         return {
             number: 25,
@@ -55,38 +12,99 @@ app.component('bubblesort', {
     methods: {
         async sort(array) {
             this.sorting = true
+
             for(var i = 0; i < array.length; i++){
                 let swaped = false
+
                 for(var j = 0; j < array.length - 1 - i; j++) {
                     await sleep(100)
                     this.selected.index = j + 1
+
                     if (array[j] > array[j + 1]) {
                         swaped = true
                         let temp = array[j]
                         array[j] = array[j + 1]
                         array[j + 1] = temp
                     }
-                    if (!this.sorting) return
+
+                    if (!this.sorting) {
+                        this.selected.index = 0
+                        return
+                    }
                     await sleep(100)
                 }
                 if (!swaped) break;
+
             }
+
             this.sorting = false
         },
+
         stop() {
             this.sorting = false
+        },
+
+        shuffle(array) {
+            for (var i = 0; i < array.length; i++) {
+                array[i] = Math.floor((Math.random() * 50) + 1)
+            }
+        },
+
+        createArray() {
+            for (var i = 0; i < this.number; i++) {
+                this.toSortArray.push(Math.floor(Math.random() * 50) + 1)
+            }
         }
     },
-    computed: {
-        array() {
-            let tmp = []
-            for (var i = 0; i < this.number; i++) {
-                tmp.push(Math.floor(Math.random() * 50) + 1)
-            }
-            this.toSortArray = tmp
-            return tmp
-        }
-    }
+    beforeMount() {
+        this.createArray()
+    },
+    template:
+    /*html*/
+    `
+    <div class="bubble">
+        <div class="graph">
+
+            <div v-for="(elments, index) in toSortArray" 
+            :style="index == selected.index ? 
+
+            { height: 10 * toSortArray[index] + 'px', width: 50 + 'px', 'background-color' : 'red', 
+            'border-width' : 0.5 + 'px', 'border-color' : 'black', 'border-style' : 'solid'}
+
+            : { height: 10 * toSortArray[index] + 'px', width: 50 + 'px', 'background-color' : '#4e4e4e', 
+            'border-width' : 0.5 + 'px', 'border-color' : 'black', 'border-style' : 'solid'}"></div>
+
+        </div>
+        <div class="options">
+            <div>
+                <label for="number">Elements in array</label>
+                <select id="number" v-model="number" :disabled="sorting">
+                    <option>25</option>
+                    <option>20</option>
+                    <option>15</option>
+                    <option>10</option>
+                    <option>5</option>
+                </select>
+            </div>
+                
+            <div>
+                <button :disabled="sorting" :class="(sorting)? 'disabledbutton' : 'button'"
+                @click="sort(this.toSortArray)">Sort!</button>
+            </div>
+            
+            <div>
+                <button :disabled="!sorting" :class="(!sorting)? 'disabledbutton' : 'button'"
+                @click="stop">Stop</button>
+            </div>
+
+            <div>
+                <button :disabled="sorting" :class="(sorting)? 'disabledbutton' : 'button'"
+                @click="shuffle(this.toSortArray)">Shuffle</button>
+            </div>
+
+        </div>
+    </div>
+    `,
 })
 
 function sleep(ms) {
--- src/index.js
+++ src/index.js
@@ -11,25 +11,3 @@ const app = Vue.createApp({
         },
     },
 })
-
-
-function loadScript(url)
-{
-    var loader = document.getElementsByTagName('body')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-    loader.appendChild(script);
-}
-
-// algorithm scrips
-// loadScript('../src/algorithms/bubbleSort.js')
-loadScript('../src/algorithms/mergeSort.js')
-
-// Does not work
-// // Component scripts
-// loadScript('../components/home.js')
-// loadScript('../components/navbar.js')
-// loadScript('../components/bubble_sort_visual.js')
-
-//app.mount("#app")
